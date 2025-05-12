46add10bba11caf0f4bb00c4550a80271edbdb7b
jsa3338 <heungnam3338@naver.com>
2025-05-12 17:59:21 +0900

    0512_ac

---
+++ source/02_HTML_CSS/ch01/2.js
@@ -0,0 +1,6 @@
+﻿// 2.js
+/* 동적인 부분 */
+var name = prompt("이름은?", "홍길동"); // 취소클릭시 name에 'null' 문자 리턴
+if (name != 'null' && name != '') { // 입력 후 확인 클릭시
+    document.write(name + '님 반갑습니다<br>');
+    }
